cab9a46b634353fb56b871b7a5cf702dcfc17f4c
Mobius-Bron <156796273+Mobius-Bron@users.noreply.github.com>
2024-09-29 09:36:16 +0800

    完善蛇的运动

--- snake/snake_body.gd
+++ snake/snake_body.gd
@@ -5,37 +5,38 @@ var dir: Vector2 = Vector2.ZERO #移动方向
 
 var nextBody = null #下一个身体部分
 var preBody = null #上一个身体部分
-
-var trace = []
+var nextPoint = null
+var lastPoint = null
 
 func _ready():
-	tracing()
-
-func tracing():
-	while true:
-		add_trace(self.global_position)
-		await get_tree().create_timer(0.1).timeout
-
-func add_trace(x):
-	if nextBody == null:
-		return
-	nextBody.trace.append(x)
+	if isHead():
+		while 1:
+			if nextPoint:
+				lastPoint = nextPoint
+			else:
+				lastPoint = self.global_position
+			nextPoint = self.global_position + dir*17
+			setNextPoint(nextBody)
+			await get_tree().create_timer(0.1).timeout
 
 func isHead():
 	return preBody == null
 
 func _physics_process(delta):
-	if isHead():
-		#移动当前的身体部分
-		dir = dir.normalized()
-		velocity = dir * moveSpeed
-		move_and_slide()
-	else:
-		var pos = (self.global_position - preBody.global_position).length()
-		#跟随前节点的路径
-		if trace.is_empty() == false:
-			if (trace[0] - self.global_position).length() < 200:
-				global_position = global_position.move_toward(trace[0], 2)
-			if (trace[0] - self.global_position).length() < 1:
-				trace.pop_front()
-	
+	if nextPoint:
+		if isHead():
+			var d = nextPoint - self.global_position
+			d.normalized()
+			velocity = d * moveSpeed / 5
+		else:
+			dir = nextPoint - self.global_position
+			dir.normalized()
+			velocity = dir * moveSpeed / 5
+	move_and_slide()
+
+func setNextPoint(x):
+	if x == null:
+		return
+	x.lastPoint = x.nextPoint
+	x.nextPoint = x.preBody.lastPoint
+	setNextPoint(x.nextBody)
